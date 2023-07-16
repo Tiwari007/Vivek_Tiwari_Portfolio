c194b62ea8e5e9f25c06d18df0d983f9c8c818f7
Vivek Tiwari <vt2174781@gmail.com>
2023-07-16 05:57:47 +0000

    (refct): pointer and colors

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import Skills from './pages/Skills';
 import DrawerAppBar from './components/NavigationBar';
 // import Footer from './pages/Footer';
 import ContactUs from './pages/ContactUs';
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <>
       <ParticleBackground />
       <div className='App'>
+        <AnimatedCursor innerSize={12}
+          outerSize={12}
+          color='255, 255, 255'
+          outerAlpha={0.5}
+          innerScale={0.7}
+          outerScale={3}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link'
+          ]} />
         <DrawerAppBar>
           <Dashboard />
           <Skills />
